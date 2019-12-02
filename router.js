274194263c185e6868644da33d842da2d6ecf54b
paulc100 <paulc100@users.noreply.github.com>
2019-12-02 14:24:53 -0800

    Version 1

--- router.js
+++ router.js
@@ -1,8 +1,15 @@
-var HomeController = require('./Controllers/HomeController');
+var PaulController = require('./Controllers/PaulController');
+var PaulController = require('./Controllers/PaulController');
+var PaulController = require('./Controllers/PaulController');
+var PaulController = require('./Controllers/PaulController');
+var PaulController = require('./Controllers/PaulController');
 
 // Routes
 module.exports = function(app){  
     // Main Routes
-    app.get('/',      HomeController.Index);    
-    app.post('/Home/ReceiveForm', HomeController.ReceiveForm);
+    app.get('/',      PaulController.PaulIndex);    
+    app.get('/Paul',      PaulController.PaulIndex);
+    app.get('/Leo',      PaulController.PaulIndex);
+    app.get('/Kraz',      PaulController.PaulIndex);
+    app.get('/Yang',      PaulController.PaulIndex);
 };
